e3c3b468df4e59ab9d199e6ac051a1d82b8b75c4
ChehabIslamTiaiba <153634800+ChehabIslamTiaiba@users.noreply.github.com>
2024-03-09 15:16:01 +0100

    Update db.js

--- api/db.js
+++ api/db.js
@@ -4,7 +4,7 @@ import mysql from "mysql2";
 export const db = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "11052004bba",
+    password: "",
     database: "healthquest",
     port: "3306",
     dateStrings: true,
